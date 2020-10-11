ae18ce9f77154ced6964c2b2cb4ad5149b31b8e3
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2020-10-11 17:03:26 +0100

    coisas

--- first.c
+++ first.c
@@ -1,11 +1,10 @@
 int main(void) {
-  char i = 'A';
+  char i = 'Ab;
   char p = '\n';
-  int do = "do";
   // this is a comment
   do
   {
-    putchar(i); /*comment*/
+    putchar(i); /*comment
     i = i + 1;
   }while (i <= 'Z')
   return 0;
