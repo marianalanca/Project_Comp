dd2832dad22193773b6f049a317614fc0efd2e8c
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2020-11-23 22:53:48 +0000

    o nosso nada

--- symbol_table.c
+++ symbol_table.c
@@ -1,16 +1,60 @@
-#ifndef SYMBOL_TABLE_H
-#define SYMBOL_TABLE_H
+#include "symbol_table.h"
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
 
-typedef enum {integer, character, doub} basic_type;
+extern table_element* symtab;
 
-typedef struct _t1{
-	char name[32];
-	basic_type type;
-	struct _t1 *next;
-} table_element;
+//Insere um novo identificador na cauda de uma lista ligada de simbolo
+table_element *insert_el(char *str, basic_type t, int line, int column)
+{
+	table_element *newSymbol=(table_element*) malloc(sizeof(table_element));
+	table_element *aux;
+	table_element* previous;
 
-table_element *insert_el(char *str, basic_type t);
-void show_table();
-table_element *search_el(char *str);
+	strcpy(newSymbol->name, str);
+	newSymbol->type=t;
+    newSymbol->line = line;
+    newSymbol->column = column;
+	newSymbol->next=NULL;
 
-#endif
+	if(symtab)	//Se table ja tem elementos
+	{	//Procura cauda da lista e verifica se simbolo ja existe (NOTA: assume-se uma tabela de simbolos globais!)
+		for(aux=symtab; aux; previous=aux, aux=aux->next)
+			if(strcmp(aux->name, str)==0)
+				return NULL;
+
+		previous->next=newSymbol;	//adiciona ao final da lista
+	}
+	else	//symtab tem um elemento -> o novo simbolo
+		symtab=newSymbol;
+
+	return newSymbol;
+}
+
+void delete_el(table_element* element){
+    free(element);
+    element=NULL;
+}
+
+void show_table()
+{
+table_element *aux;
+printf("\n");
+printf("===== Function main Symbol Table =====\n");
+for(aux=symtab; aux; aux=aux->next)
+	printf("%s\t%d\n", aux->name, aux->type);
+}
+
+//Procura um identificador, devolve 0 caso nao exista
+table_element *search_el(char *str)
+{
+table_element *aux;
+
+for(aux=symtab; aux; aux=aux->next)
+	if(strcmp(aux->name, str)==0)
+		return aux;
+
+return NULL;
+}
+
